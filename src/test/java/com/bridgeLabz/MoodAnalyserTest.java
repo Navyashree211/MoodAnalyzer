c8e29e7f0e1f983964153ac65fc9ebd880d93759
navyas211@gmail.com <navyas211@gmail.com>
2021-10-18 21:41:42 +0530

    [Refactor]: refactor code

--- src/test/java/com/bridgeLabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgeLabz/MoodAnalyserTest.java
@@ -1,20 +1,21 @@
 package com.bridgeLabz;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
 
-    @Test
-    public void givenMoodShouldReturnHappy() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Happy Mood");
-            String mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("HAPPY", mood);
-    }
+	@Test
+	public void whenMood_IsSad_ShouldReturnSad() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("SAD", mood);
+	}
 
-    @Test
-    public void givenMoodShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
-            String mood = moodAnalyser.analyseMood();
-            Assert.assertEquals("SAD", mood);
-    }
+	@Test
+	public void whenIn_AnyMood_ShouldReturnHappy() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+		String mood = moodAnalyser.analyseMood();
+		Assert.assertEquals("HAPPY", mood);
+	}
 }
